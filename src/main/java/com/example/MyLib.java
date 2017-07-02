0eda8a0be042684b458117b46e9ae4e626706af4
Or Shachar <ors@wix.com>
2017-07-02 18:26:55 +0300

    added target that would show warning on first run

---
+++ src/main/java/com/example/MyLib.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class MyLib {
+
+	private static double x = 3.4;
+    
+    void test() {
+      String a = "hello" + new char[]{'w','o','r','l','d'};
+      this.x = this.x + 2.1;
+    }
+}
