d29a3807376468b8b7e5cde5008c0f46b4a4f977
Or Shachar <ors@wix.com>
2018-07-04 15:36:41 +0300

    added member that only exists on oracle jdk

--- src/main/java/com/example/MyTest.java
+++ src/main/java/com/example/MyTest.java
@@ -2,12 +2,14 @@ package com.example;
 
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-
+import javafx.util.Pair;
 
 public class MyTest {
 
     @Test
     public void test() throws InterruptedException{
+    	Pair<String,String> p = new Pair<>("hello","world");
+    	System.out.println(p);
         System.out.println(System.getProperty("java.vendor"));
         System.out.println(System.getProperty("java.version"));
         Thread.sleep(5000);
